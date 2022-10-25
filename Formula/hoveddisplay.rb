f8f8e7166003b50a1ec64a538587a1234ba15f7c
Rahul Tarak <cryogenicplanet@gmail.com>
2022-10-25 13:53:14 +0530

    Create cryogenicplanet/hoveddisplay tap

---
+++ Formula/hoveddisplay.rb
@@ -0,0 +1,23 @@
+class Hovedisplay < Formula
+  desc "macOS command-line utility to configure main display"
+  homepage "https://github.com/CryogenicPlanet/hovedisplay"
+  url "https://github.com/CryogenicPlanet/hovedisplay/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "da0a5d73cc9c704c9e7ba9ee512fba5f8d3136979f2560ad25438608b84bbe27"
+  license "MIT"
+
+  depends_on xcode: [">= 11.2", :build]
+  depends_on macos: [
+    :catalina,
+    :big_sur,
+  ]
+
+  def install
+    cd "src-swift"
+    system "swift", "build", "--disable-sandbox", "--configuration", "release"
+    bin.install ".build/release/hoveddisplay"
+  end
+
+  test do
+    system "hoveddisplay", "list"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Cryogenicplanet Hoveddisplay
+
+## How do I install these formulae?
+
+`brew install cryogenicplanet/hoveddisplay/<formula>`
+
+Or `brew tap cryogenicplanet/hoveddisplay` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
