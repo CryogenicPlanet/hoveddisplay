c92c49b001eaeb484a5f863c93dc576175c4411d
Rahul Tarak <cryogenicplanet@gmail.com>
2022-10-25 14:27:05 +0530

    🐛 Fix name of package

--- Formula/hoveddisplay.rb
+++ Formula/hoveddisplay.rb
@@ -1,7 +1,7 @@
-class Hovedisplay < Formula
+class Hoveddisplay < Formula
   desc "macOS command-line utility to configure main display"
-  homepage "https://github.com/CryogenicPlanet/hovedisplay"
-  url "https://github.com/CryogenicPlanet/hovedisplay/archive/refs/tags/v0.0.1.tar.gz"
+  homepage "https://github.com/CryogenicPlanet/hoveddisplay"
+  url "https://github.com/CryogenicPlanet/hoveddisplay/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "da0a5d73cc9c704c9e7ba9ee512fba5f8d3136979f2560ad25438608b84bbe27"
   license "MIT"
 
